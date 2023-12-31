66a0d92db6bc558aed66b0d338419841aa725cfb
Y.Ninomiya(kadoma) <kadoma.fukakusa248@gmail.com>
2024-01-01 00:42:09 +0900

    refactor(roulette): some fix and changes for 2024

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 let container = document.querySelector(".container");
 let btn = document.getElementById("spin");
-// 乱数を生成
-let number = Math.ceil(Math.random() * 1000 * 5);
+
+// 初回乱数を生成
+let number = Math.ceil(Math.random() * 1000 * 2);
+container.style.transform = "rotate(" + number + "deg)";
 
 btn.onclick = function () {
-  //乱数だけ回転
-  container.style.transform = "rotate(" + number + "deg)";
   //乱数を新しくする
   number += Math.ceil(Math.random() * 1000 * 4);
+  container.style.transform = "rotate(" + number + "deg)";
 };
